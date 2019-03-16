d0a2758dc7004019406126c7a9b4992b5c825127
Alex Lloyd <lloyd.da89@gmail.com>
2019-03-16 12:23:32 -0400

    Updates

---
+++ precache-manifest.37566b142349b174b4c52507bb73ad04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0535c897ab70a2e08cdece2d6857231a",
+    "url": "/Click-Game/static/media/background.0535c897.png"
+  },
+  {
+    "revision": "73c4ac620ebd606b1f98",
+    "url": "/Click-Game/static/js/runtime~main.2ae5b6b9.js"
+  },
+  {
+    "revision": "3925a34d242b7eee8d6c",
+    "url": "/Click-Game/static/js/main.47300ef9.chunk.js"
+  },
+  {
+    "revision": "ffee2253efef96f7949d",
+    "url": "/Click-Game/static/js/2.6dc0ac8b.chunk.js"
+  },
+  {
+    "revision": "3925a34d242b7eee8d6c",
+    "url": "/Click-Game/static/css/main.5e85d204.chunk.css"
+  },
+  {
+    "revision": "70678068b8f67bd82fafda9ac03aa070",
+    "url": "/Click-Game/index.html"
+  }
+];
